53cac42db3e8d5640f310a6a96635408468bbf04
calvinrodrigues500 <calvinrodrigues500@gmail.com>
2024-10-13 20:37:48 +0530

    Initial commit

---
+++ src/main/java/com/calvin/log_analytics_server/LogAnalyticsServerApplication.java
@@ -0,0 +1,13 @@
+package com.calvin.log_analytics_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyticsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyticsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calvin/log_analytics_server/LogAnalyticsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calvin.log_analytics_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyticsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
